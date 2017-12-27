8a8d34b135a2c9fea49df8b2d587977162b7b091
SvetoslavKadiev <svetoslav.kadiev@gmail.com>
2017-12-27 20:50:57 +0200

    Add Journal class

--- SingleResponsibility/Program.cs
+++ SingleResponsibility/Program.cs
@@ -6,46 +6,7 @@ using static System.Console;
 
 namespace SingleResponsibility
 {
-    // just stores a couple of journal entries and ways of
-    // working with them
-    public class Journal
-    {
-        private readonly List<string> entries = new List<string>();
-
-        private static int count = 0;
-
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count; // memento pattern!
-        }
-
-        public void RemoveEntry(int index)
-        {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-
-        // breaks single responsibility principle
-        public void Save(string filename, bool overwrite = false)
-        {
-            File.WriteAllText(filename, ToString());
-        }
-
-        public void Load(string filename)
-        {
-
-        }
-
-        public void Load(Uri uri)
-        {
-
-        }
-    }
+    
 
     // handles the responsibility of persisting objects
     public class Persistence
