6e4feda0d08b5635b27c9efe1a3d92153ef7efab
SvetoslavKadiev <svetoslav.kadiev@gmail.com>
2017-12-27 21:44:48 +0200

    Implement Liskov Substitution

--- LiskovSubstituion/Program.cs
+++ LiskovSubstituion/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static  System.Console;
 
 namespace LiskovSubstituion
 {
     class Program
     {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
         static void Main(string[] args)
         {
+            Rectangle rc = new Rectangle(2, 3);
+            WriteLine($"{rc} has area {Area(rc)}");
+
+
+            // should be able to substitute a base type for a subtype
+            /*Square*/
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            WriteLine($"{sq} has area {Area(sq)}");
+
+            ReadLine();
         }
     }
 }
